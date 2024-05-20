013262c3cd66a36b8b341d9f696aba5abd5d3f55
grimSkepsis <developer.mikehofer@gmail.com>
2024-05-20 08:42:16 -0700

    adds variant combination

--- src/main.rs
+++ src/main.rs
@@ -78,7 +78,16 @@ async fn main() {
     let pass = env::var("NEO4J_PASSWORD").unwrap_or_else(|_| "neo4j".to_string());
     let graph = Graph::new(uri, user, pass).await.unwrap();
 
-    let query = "MATCH (p:Character {name: $name})-[:OWNS]->(inv:Inventory)-[:CONTAINS]->(item:Variant) RETURN item.name, item.effect";
+    let query =
+        "MATCH (p:Character {name: $name})-[:OWNS]->(inv:Inventory)-[:CONTAINS]->(item:Item)
+        OPTIONAL MATCH (item)-[:HAS_BASE]->(baseItem:ItemBase)
+        RETURN
+        CASE WHEN baseItem.name IS NOT NULL
+             THEN baseItem.name + ' (' + item.name + ')'
+             ELSE item.name
+        END AS name,
+        COALESCE(item.description, baseItem.description) as description,
+        item.effect as effect";
 
     // Set your parameters
     let parameters = neo4rs::query(query).param("name", "Thorrun");
@@ -89,20 +98,17 @@ async fn main() {
     // Iterate and process the results
     while let Ok(Some(row)) = result.next().await {
         // Log the raw row for debugging
-        println!("{:?}", row);
+        // println!("{:?}", row);
 
         // Safely extract the fields
-        let name: Result<String, _> = row.get("item.name");
+        let name: String = row.get("name").unwrap();
         // let description: Result<String, _> = row.get("description");
-        let effect: Result<String, _> = row.get("item.effect");
-        match (name, effect) {
-            (Ok(name), Ok(effect)) => {
-                println!("Item: {}, Effect: {}", name, effect);
-            }
-            (Err(e), _) | (_, Err(e)) => {
-                eprintln!("Error extracting row: {:?}", e);
-            }
-        }
+        let description: String = row.get("description").unwrap_or_else(|_| "N/A".to_string());
+        let effect: String = row.get("effect").unwrap_or_else(|_| "N/A".to_string());
+        println!(
+            "Item: {}, Effect: {}, Description: {}",
+            name, effect, description
+        );
     }
 
     println!("GQL on: {host}");
