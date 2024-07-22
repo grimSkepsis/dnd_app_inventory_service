2ef37f83c5d9f217edd5f909a33c7916c1647b36
grimSkepsis <developer.mikehofer@gmail.com>
2024-07-22 09:57:47 -0700

    removes object in favor of simple object for paginated type

--- src/graphql/schemas/paginated_response_schema.rs
+++ src/graphql/schemas/paginated_response_schema.rs
@@ -1,33 +1,10 @@
-use async_graphql::{Object, OutputType};
+use async_graphql::{OutputType, SimpleObject};
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, SimpleObject)]
 pub struct PaginatedResponse<T: OutputType + Sync> {
     pub entities: Vec<T>,
     pub total_entities: u32,
     pub page_index: u32,
     pub page_size: u32,
     pub total_pages: u32,
-}
-
-#[Object]
-impl<T: OutputType + Sync> PaginatedResponse<T> {
-    async fn entities(&self) -> &Vec<T> {
-        &self.entities
-    }
-
-    async fn total_entities(&self) -> u32 {
-        self.total_entities
-    }
-
-    async fn page_index(&self) -> u32 {
-        self.page_index
-    }
-
-    async fn page_size(&self) -> u32 {
-        self.page_size
-    }
-
-    async fn total_pages(&self) -> u32 {
-        self.total_pages
-    }
 }
